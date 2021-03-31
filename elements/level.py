de109731339e7e2c9a79ad8d92b7195b232f931a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-31 11:57:21 +0200

    refactor(test): move doctests to cleanup method header

--- elements/level.py
+++ elements/level.py
@@ -6,10 +6,6 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-"""\
->>> level_numbered('a. Gesamtbewertung')
-4
-"""
 
 import re
 
@@ -19,21 +15,12 @@ def level_numbered(raw: str) -> int:
 
     >>> level_numbered('5 Geology')
     1
-    >>> level_numbered('2. Zentrum')
-    1
     >>> level_numbered('2.1.3. Abschluss')
     3
-    >>> level_numbered('2.1 Anhang')
-    2
-    >>> level_numbered('2..1... Fehlerfrei') # ignore typos
-    2
-    >>> level_numbered('2020 This is not a headline level')
-    False
-    >>> level_numbered('04.03.2016. No Headline')
-    False
+    >>> level_numbered('a. Gesamtbewertung')
+    4
     """
     # TODO: SUPPORT LEVEL WITHOUT SPACE
-    # TODO: MOVE TESTS?
     raw = raw.strip()
     if not raw:
         return None
---
+++ tests/test_level.py
@@ -0,0 +1,45 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+import utila
+
+import elements
+
+HEADLINE_LEVEL = """\
+1   2. Zentrum
+2   2.1 Anhang
+2   2..1... Fehlerfrei
+""".splitlines()
+
+
+@pytest.mark.parametrize('line', [
+    pytest.param(item, id=str(index))
+    for index, item in enumerate(HEADLINE_LEVEL)
+])
+def test_headline_level(line):
+    expected, raw = line.split(maxsplit=1)
+    expected = int(expected)
+    level = elements.level_numbered(raw)
+    assert level == expected
+
+
+NOHEADLINE_LEVEL = """\
+2020 This is not a headline level
+04.03.2016. No Headline
+""".splitlines()
+
+
+@pytest.mark.parametrize('noheadline', [
+    pytest.param(item, id=str(index))
+    for index, item in enumerate(NOHEADLINE_LEVEL)
+])
+def test_noheadline_level(noheadline):
+    nolevel = elements.level_numbered(noheadline)
+    assert nolevel is False
