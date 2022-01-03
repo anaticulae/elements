94004cdf1d74ccbbc9ae644e5c6c71a49675fcc8
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-03 14:24:46 +0100

    0.12.1 auto generated release

--- elements/__init__.py
+++ elements/__init__.py
@@ -31,6 +31,6 @@ from elements.quote import isquote
 from elements.toc import istoc
 from elements.toc import toc_flat
 
-__version__ = '0.12.0'
+__version__ = '0.12.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
